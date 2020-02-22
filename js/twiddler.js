c46e78e8ed40e97f4841c9c91b058e94506266d7
Jeff Maughan <geophray@gmail.com>
2020-02-22 13:34:03 -0700

    Move all javascript to js/
    Moved all javascript to a separate file for easier maintainability.
    Also created basic file structure for index.html and altered original js
    to target #feed instead of body so that I have better control over what
    part of the feed is updated by js and what is persistent.

---
+++ js/twiddler.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var $feed = $('#feed');
+    $feed.html('');
+
+    var index = streams.home.length - 1;
+    while(index >= 0){
+      var tweet = streams.home[index];
+      var $tweet = $('<div></div>');
+      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      $tweet.appendTo($feed);
+      index -= 1;
+    }
+
+  });

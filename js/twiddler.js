feb354c3bc8d60da419997b8887f7a998a63ae55
Jeff Maughan <geophray@gmail.com>
2020-02-22 17:21:31 -0700

    Add css

--- js/twiddler.js
+++ js/twiddler.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var $feed = $('#feed');
     $feed.html('');
 
     var index = streams.home.length - 1;
-    while(index >= 0){
-      var tweet = streams.home[index];
-      var $tweet = $('<div></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message);
-      $tweet.appendTo($feed);
-      index -= 1;
+    while (index >= 0) {
+        var tweet = streams.home[index];
+        var $tweet = $('<div class="twiddle"></div>');
+        $tweet.text('@' + tweet.user + ': ' + tweet.message);
+        $tweet.appendTo($feed);
+        index -= 1;
     }
 
-  });
+});
